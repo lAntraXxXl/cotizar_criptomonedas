4c0ec1698a5d8153c0b66d1cc4c2e23fb0678131
lAntraXxXl <79475476+lAntraXxXl@users.noreply.github.com>
2021-02-28 15:23:37 -0600

    Updates

---
+++ precache-manifest.48373a5d82120b93aaa82e9f8127589e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db15dc088239c661b6a3c2c625703d9",
+    "url": "/cotizar_criptomonedas/index.html"
+  },
+  {
+    "revision": "c2118faca6b04100514f",
+    "url": "/cotizar_criptomonedas/static/css/main.3ed0f328.chunk.css"
+  },
+  {
+    "revision": "cf1d22309c03d9fe5d36",
+    "url": "/cotizar_criptomonedas/static/js/2.ebe1bf62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cotizar_criptomonedas/static/js/2.ebe1bf62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2118faca6b04100514f",
+    "url": "/cotizar_criptomonedas/static/js/main.396e1786.chunk.js"
+  },
+  {
+    "revision": "419afd7ac026b893012d",
+    "url": "/cotizar_criptomonedas/static/js/runtime-main.30191dfa.js"
+  }
+]);
